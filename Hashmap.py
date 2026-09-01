cc6da116d6daa51d91b4371f364c124d35ead545
Carlson <soocarlson6@gmail.com>
2026-09-02 00:04:29 +0800

    Initialise Methods

--- Hashmap.py
+++ Hashmap.py
@@ -6,4 +6,42 @@ class HashMap:
         self.size = 0
         self.buckets = [[] for _ in range(capacity)]
 
-    
+    # Adding Methods
+
+    # 1. Length
+    def __len__(self):
+        pass
+
+    # 2. Check if an item is part of the hashmap
+    def __contains__(self, key):
+        pass
+
+    # 3. Putting in a new key-value pair
+    def put(self, key, value):
+        pass
+
+    # 4. Obtain the value of a provided key
+    def get(self, key):
+        pass
+
+    # 5. Remove a key 
+    def remove(self, key):
+        pass
+
+    # 6. Dictionary methods
+    # 6.1 keys
+    def keys(self):
+        pass
+
+    # 6.2 values
+    def values(self):
+        pass
+
+    # 6.3 key-value pairs
+    def items(self):
+        pass
+
+    # Hash Function - helper function
+    def _hash_function(self, key):
+        pass
+
